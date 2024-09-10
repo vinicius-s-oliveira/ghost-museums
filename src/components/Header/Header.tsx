03c4165596faac42d311079700725645008ef0f4
vinicius.s.oliveira <vinicius.s.oliveira@accenture.com>
2024-09-10 17:51:58 -0300

    feat: adds styling to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,44 +2,34 @@ import styles from "./Header.module.scss";
 
 export default function Header() {
   return (
-    <>
-      <header className={styles.header}>
-        <div className={styles.wrapperCenter}>
-          <a href="/">
-            <img
-              className={styles.logo}
-              src="./src/assets/museum-icon.png"
-              alt="logo-icon"
-            ></img>
-          </a>
+    <header className={styles.header}>
+      <div className={styles.wrapperCenter}>
+        <a href="/">
+          <img
+            className={styles.logo}
+            src="./src/assets/museum-icon.png"
+            alt="logo-icon"
+          ></img>
+        </a>
 
-          <nav>
-            <ul>
-              <li>
-                <a
-                  className="bg-neutral-300 decoration-charts-carrot-300"
-                  href="#"
-                >
-                  Localidades
-                </a>
-              </li>
+        <nav className={styles.navBar}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <a href="#">Localidades</a>
+            </li>
 
-              <li>
-                <a href="#">Seja um Parceiro</a>
-              </li>
+            <li className={styles.navItem}>
+              <a href="#">Seja um Parceiro</a>
+            </li>
 
-              <li>
-                <a href="#">Entrar</a>
-              </li>
-            </ul>
-
-            {/* @TO-DO: Login Icon */}
-            {/* <a href="/">
-              <img src="" alt="login-icon"></img>
-            </a> */}
-          </nav>
-        </div>
-      </header>
-    </>
+            <li className={styles.navItem}>
+              <button className={styles.button} type="button">
+                Entrar
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
