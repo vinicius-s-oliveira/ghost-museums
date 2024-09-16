4231ef15868849bed7a067818e7230bb3b76bc5b
vinicius.s.oliveira <vinicius.s.oliveira@accenture.com>
2024-09-16 09:38:02 -0300

    feat: refactors unnecessary import usage and button component

--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 
-// import styles from "./styles.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
